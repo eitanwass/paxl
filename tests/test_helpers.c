fbf590f430528690ec7abde47721cb36f7ef74b5
Ethan Wass <eitanwass@gmail.com>
2026-02-04 21:53:55 +0200

    Add helpers tests

--- tests/test_helpers.c
+++ tests/test_helpers.c
@@ -1,10 +1,70 @@
 #include "unity.h"
+#include "../src/core/helpers.h"
 
 void setUp(void) {}
 void tearDown(void) {}
 
+
+// skip_ws
+
+void test_skip_ws_leading_spaces(void) {
+    char str[] = "   hello";
+    char* result = skip_ws(str);
+    TEST_ASSERT_EQUAL_STRING("hello", result);
+}
+
+void test_skip_ws_suffix_spaces(void) {
+    char str[] = "hello   ";
+    char* result = skip_ws(str);
+    TEST_ASSERT_EQUAL_STRING("hello   ", result);
+}
+
+void test_skip_ws_no_spaces(void) {
+    char str[] = "hello";
+    char* result = skip_ws(str);
+    TEST_ASSERT_EQUAL_STRING("hello", result);
+}
+
+void test_skip_ws_all_spaces(void) {
+    char str[] = "   ";
+    char* result = skip_ws(str);
+    TEST_ASSERT_EQUAL_STRING("", result);
+}
+
+void test_skip_ws_empty_string(void) {
+    char str[] = "";
+    char* result = skip_ws(str);
+    TEST_ASSERT_EQUAL_STRING("", result);
+}
+
+// skip_to_tag_end
+
+void test_skip_to_tag_end_with_closing(void) {
+    char str[] = "<tag>";
+    char* result = skip_to_tag_end(str);
+    TEST_ASSERT_EQUAL_STRING("", result);
+}
+
+void test_skip_to_tag_end_without_closing(void) {
+    char str[] = "<tag";
+    char* result = skip_to_tag_end(str);
+    TEST_ASSERT_EQUAL_STRING("", result);
+}
+
+void test_skip_to_tag_end_empty(void) {
+    char str[] = "";
+    char* result = skip_to_tag_end(str);
+    TEST_ASSERT_EQUAL_STRING("", result);
+}
+
 int main(void) {
     UNITY_BEGIN();
-    // Add tests here
+    RUN_TEST(test_skip_ws_leading_spaces);
+    RUN_TEST(test_skip_ws_no_spaces);
+    RUN_TEST(test_skip_ws_all_spaces);
+    RUN_TEST(test_skip_ws_empty_string);
+    RUN_TEST(test_skip_to_tag_end_with_closing);
+    RUN_TEST(test_skip_to_tag_end_without_closing);
+    RUN_TEST(test_skip_to_tag_end_empty);
     return UNITY_END();
 }
